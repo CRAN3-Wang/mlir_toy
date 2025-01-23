488904dfeffc9ac5e4440d66f3b9908e45590e78
heru.wang@duke.edu <heru.wang@duke.edu>
2025-01-24 00:45:40 +0800

    Toy Language and AST

---
+++ build/toy/CMakeFiles/toyc.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/crane/dev/mlir_toy/toy/src/AST.cpp" "toy/CMakeFiles/toyc.dir/src/AST.cpp.o" "gcc" "toy/CMakeFiles/toyc.dir/src/AST.cpp.o.d"
+  "/home/crane/dev/mlir_toy/toy/src/toyc.cpp" "toy/CMakeFiles/toyc.dir/src/toyc.cpp.o" "gcc" "toy/CMakeFiles/toyc.dir/src/toyc.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/toy/CMakeFiles/toyc.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/toyc.dir/src/AST.cpp.o"
+  "CMakeFiles/toyc.dir/src/AST.cpp.o.d"
+  "CMakeFiles/toyc.dir/src/toyc.cpp.o"
+  "CMakeFiles/toyc.dir/src/toyc.cpp.o.d"
+  "toyc"
+  "toyc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/toyc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
