1f2a3be80ff6c03eb9d07b11bb076d263afd5407
heru.wang@duke.edu <heru.wang@duke.edu>
2025-01-23 17:41:05 +0800

    config done

---
+++ build/ch1_io/CMakeFiles/ch1_io.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/crane/dev/mlir_toy/ch1_io/ch1_io.cpp" "ch1_io/CMakeFiles/ch1_io.dir/ch1_io.cpp.o" "gcc" "ch1_io/CMakeFiles/ch1_io.dir/ch1_io.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/ch1_io/CMakeFiles/ch1_io.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ch1_io.
+# This may be replaced when dependencies are built.
